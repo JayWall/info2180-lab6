3af4b250f59c851d309a59f572d526d620cea1fb
JayWall <sanjah.hall@gmail.com>
2016-11-11 17:13:43 -0500

    Exercise 4

--- request.php
+++ request.php
@@ -1,18 +1,37 @@
 <?php
 
-// accept a term (keyword)
-// respond with a value
+$xmldata = '<?xml version="1.0" encoding="UTF-8"?>
+<input>
+    <defintion name="definition" author="Captain America">
+        A statement of the exact meaning of a word, especially in a dictionary.
+    </definition>
+    
+    <defintion name="bar" author="Spiderman">
+        A place that sells alcholic beverages
+    </definition>
+    
+    <defintion name="ajax" author="Ironman">
+        Technique which involves the use of javascript and xml (or JSON)
+    </definition>
+    
+    <defintion name="html" author="Black Widow">
+        The standard markup language for creating web pages and web applications.
+    </definition>
+    
+    <defintion name="css" author="Storm">
+        A style sheet language used for describing the presentation of a document written in a markup language.
+    </definition>
+    
+    <defintion name="javascript" author="Batman">
+        A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.
+    </definition>
+    
+    <defintion name="php" author="Flash">
+        <name>Storm</name>
+        <email>storm@example.com</email>
+    </definition>
+</input>';
 
-$query = $_GET['q'];
-$definition = [
-    "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
-    "html" => "The standard markup language for creating web pages and web applications.",
-    "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
-    "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
-];
-
-print "<h3>" . strtoupper($query) . "</h3>";
-print "<p>" . $definition[$query] . "</p>";
+header('Content-Type: text/xml');
+$xmlOutput = new SimpleXMLElement($xmldata);
+echo $xmlOutput->asXML();
